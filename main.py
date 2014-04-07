a1b432fbdd5cc478e0a74d4c26f20186754b38c2
Kelvinm <kelvin.malyar@gmail.com>
2014-04-07 00:10:16 -0400

    main and packages

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+import lxml.html as lh
+import datetime
+import requests
+import cssselect
+
+
+def main():
+    """docstring for main"""
+    print 'hello world'
+    
+if __name__ == '__main__':
+    main()
